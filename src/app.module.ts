038a1a57d5b9bd93374c07906f22bbf1928cd0dd
FleeingSunlight <gh@fleeingsunlight.dev>
2021-02-21 13:10:40 +0700

    feat: init modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AccountModule } from './account/account.module';
+import { OrganizerModule } from './organizer/organizer.module';
+import { ParticipantModule } from './participant/participant.module';
+import { FacilityModule } from './facility/facility.module';
 
 @Module({
-  imports: [],
+  imports: [AccountModule, OrganizerModule, ParticipantModule, FacilityModule],
   controllers: [AppController],
   providers: [AppService],
 })
