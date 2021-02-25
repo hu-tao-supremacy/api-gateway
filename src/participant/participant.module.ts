181f651fb52eae9cbe836016022a267ce8b87a3f
FleeingSunlight <gh@fleeingsunlight.dev>
2021-02-25 19:49:36 +0700

    [participant] Remove unused import statements

--- src/participant/participant.module.ts
+++ src/participant/participant.module.ts
@@ -1,7 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { HTS_PARTICIPANT_PACKAGE_NAME } from 'apis/gen/nest/hts/participant/service';
-import { join } from 'path';
 import { ParticipantService } from './participant.service';
 
 @Module({
