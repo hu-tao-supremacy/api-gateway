584ab790cf8672a668c5047d84535e93ee06d10f
FleeingSunlight <gh@fleeingsunlight.dev>
2021-02-25 10:06:50 +0700

    [account] Generate GraphQL resolver

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { AccountResolver } from './account.resolver';
 
-@Module({})
+@Module({
+  providers: [AccountResolver]
+})
 export class AccountModule {}
