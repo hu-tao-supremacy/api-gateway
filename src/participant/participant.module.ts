2a22d97747fd924bb24c4e2fbfcf5cf83c4960af
FleeingSunlight <gh@fleeingsunlight.dev>
2021-02-25 19:48:28 +0700

    [participant] Remove participant.resolver.ts

--- src/participant/participant.module.ts
+++ src/participant/participant.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { HTS_PARTICIPANT_PACKAGE_NAME } from 'apis/gen/nest/hts/participant/service';
 import { join } from 'path';
-import { ParticipantResolver } from './participant.resolver';
 import { ParticipantService } from './participant.service';
 
 @Module({
   imports: [],
-  providers: [ParticipantResolver, ParticipantService],
+  providers: [ParticipantService],
 })
 export class ParticipantModule {}
