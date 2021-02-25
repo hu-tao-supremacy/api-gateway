3e11089aa26b3ee30bc89d05b567a2ccbc579991
FleeingSunlight <gh@fleeingsunlight.dev>
2021-02-25 12:22:04 +0700

    [account] Implement getProfile() in AccountResolver

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -1,7 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { HTS_ACCOUNT_PACKAGE_NAME } from 'apis/gen/nest/hts/account/service';
+import { join } from 'path';
 import { AccountResolver } from './account.resolver';
+import { AccountService } from './account.service';
 
 @Module({
-  providers: [AccountResolver]
+  imports: [
+    ClientsModule.register([
+      {
+        name: HTS_ACCOUNT_PACKAGE_NAME,
+        transport: Transport.GRPC,
+        options: {
+          package: HTS_ACCOUNT_PACKAGE_NAME,
+          protoPath: join(
+            __dirname,
+            '../../../apis/proto/hts/account/service.proto',
+          ),
+          loader: {
+            includeDirs: [join(__dirname, '../../../apis/proto')],
+          },
+        },
+      },
+    ]),
+  ],
+  providers: [AccountResolver, AccountService],
 })
 export class AccountModule {}
--- src/account/account.resolver.ts
+++ src/account/account.resolver.ts
@@ -1,4 +1,20 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Resolver, Query } from '@nestjs/graphql';
+import { Account } from 'src/models/account.model';
+import { AccountService } from './account.service';
+import { Result } from 'src/models/result.model';
 
-@Resolver()
-export class AccountResolver {}
+@Resolver((_) => Account)
+export class AccountResolver {
+  constructor(private readonly accountService: AccountService) {}
+
+  @Query((_) => Result)
+  async getProfile(
+    @Args('access_token', { type: () => String }) accessToken: string,
+  ) {
+    let ret = await this.accountService.isAuthenticated(accessToken);
+    return {
+      isOk: ret.isOk,
+      description: ret.description,
+    };
+  }
+}
