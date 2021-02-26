fe4608fd3a1a7f9e046aa081e5fca724b1051a9a
FleeingSunlight <gh@fleeingsunlight.dev>
2021-02-26 10:38:30 +0700

    [account] Import EnvoyAccountModule into AccountModule

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AccountService } from './account.service';
 import { AccountResolver } from './account.resolver';
+import { EnvoyAccountModule } from 'src/envoy-account/envoy-account.module';
+import { EnvoyAccountService } from 'src/envoy-account/envoy-account.service';
 
 @Module({
-  providers: [AccountService, AccountResolver]
+  imports: [EnvoyAccountModule],
+  providers: [EnvoyAccountService, AccountService, AccountResolver],
 })
 export class AccountModule {}
--- src/account/account.resolver.ts
+++ src/account/account.resolver.ts
@@ -1,4 +1,8 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
+import { EnvoyAccountService } from 'src/envoy-account/envoy-account.service';
+import { Account } from 'src/models/account.model';
 
-@Resolver()
-export class AccountResolver {}
+@Resolver((_) => Account)
+export class AccountResolver {
+  constructor(private readonly envoyAccountService: EnvoyAccountService) {}
+}
