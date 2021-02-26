3d1f7258347e08e4f2119e3b67cbe68812e8fbad
FleeingSunlight <gh@fleeingsunlight.dev>
2021-02-26 08:52:40 +0700

    [app] Update import statements

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AccountModule } from './account/account.module';
-import { OrganizerModule } from './organizer/organizer.module';
-import { ParticipantModule } from './participant/participant.module';
 import { FacilityModule } from './facility/facility.module';
 import { GraphQLModule } from '@nestjs/graphql';
-import { EventModule } from './event/event.module';
 import { GlobalModule } from './global/global.module';
 import { EventModule } from './resolvers/event/event.module';
 import { AccountModule } from './resolvers/account/account.module';
-import { AccountModule } from './proxies/account/account.module';
-import { ParticipantModule } from './proxies/participant/participant.module';
-import { EventModule } from './proxies/event/event.module';
-import { OrganizerModule } from './proxies/organizer/organizer.module';
+import { AccountModule as ProxyAccountModule } from './proxies/account/account.module';
+import { ParticipantModule as ProxyParticipantModule } from './proxies/participant/participant.module';
+import { EventModule as ProxyEventModule } from './proxies/event/event.module';
+import { OrganizerModule as ProxyOrganizerModule } from './proxies/organizer/organizer.module';
 
 @Module({
   imports: [
@@ -22,12 +18,18 @@ import { OrganizerModule } from './proxies/organizer/organizer.module';
       debug: process.env.NODE_ENV === 'dev',
       playground: process.env.NODE_ENV === 'dev',
     }),
-    AccountModule,
-    OrganizerModule,
-    ParticipantModule,
-    FacilityModule,
-    EventModule,
+
     GlobalModule,
+
+    // Proxies
+    ProxyAccountModule,
+    ProxyParticipantModule,
+    ProxyEventModule,
+    ProxyOrganizerModule,
+
+    // Resolvers
+    EventModule,
+    AccountModule,
   ],
   controllers: [AppController],
   providers: [AppService],
