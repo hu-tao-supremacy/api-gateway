90fd35416649a023dcad0d2b6c06758ef73e5315
FleeingSunlight <gh@fleeingsunlight.dev>
2021-02-26 09:51:20 +0700

    [auth] Add serviceValidation method to AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async serviceValidation(
+    accessToken: string,
+  ): Promise<ChulaSSOSuccessResponse> {
+    return new Promise((resolve, _) => {
+      setTimeout(() => {
+        resolve({
+          uid: '0001',
+          username: 'test',
+          gecos: 'Test Account',
+          email: 'test@onepass.app',
+          roles: ['faculty', 'student'],
+          ouid: '1234567890',
+        });
+      }, 400);
+    });
+  }
+}
+
+interface ChulaSSOSuccessResponse {
+  uid: string;
+  username: string;
+  gecos: string;
+  email: string;
+  roles: string[];
+  ouid: string;
+}
+
+interface ChulaSSOFailureResponse {
+  type: string;
+  content: string;
+}
