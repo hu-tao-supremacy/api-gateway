ae6e498c7ed5cf4a20a87cb806984ba4ffe0335f
FleeingSunlight <gh@fleeingsunlight.dev>
2021-02-26 12:50:45 +0700

    [envoy-organizer] Implement getOrganizationById()

--- src/envoy-organizer/envoy-organizer.module.ts
+++ src/envoy-organizer/envoy-organizer.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { EnvoyOrganizerService } from './envoy-organizer.service';
 
 @Module({
-  providers: [EnvoyOrganizerService]
+  providers: [EnvoyOrganizerService],
+  exports: [EnvoyOrganizerService],
 })
 export class EnvoyOrganizerModule {}
--- src/envoy-organizer/envoy-organizer.service.ts
+++ src/envoy-organizer/envoy-organizer.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+import {
+  HTS_ORGANIZER_PACKAGE_NAME,
+  OrganizationServiceClient,
+  ORGANIZATION_SERVICE_NAME,
+} from '../apis/hts/organizer/service';
+import * as common from '../apis/hts/common/common';
+import { from, Observable, of } from 'rxjs';
 
 @Injectable()
-export class EnvoyOrganizerService {}
+export class EnvoyOrganizerService implements OnModuleInit {
+  private organizerService: OrganizationServiceClient;
+
+  constructor(@Inject(HTS_ORGANIZER_PACKAGE_NAME) private client: ClientGrpc) {}
+
+  onModuleInit() {
+    this.organizerService = this.client.getService<OrganizationServiceClient>(
+      ORGANIZATION_SERVICE_NAME,
+    );
+  }
+
+  getOrganizationById(organizationId: number): Observable<common.Organization> {
+    let organization: common.Organization = {
+      id: organizationId,
+      name: 'HU TAO SUPREMACY',
+      isVerified: true,
+    };
+
+    return from([organization]);
+  }
+}
