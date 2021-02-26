91db9ca81d3b139a1a92825420b9d3afa065c591
FleeingSunlight <gh@fleeingsunlight.dev>
2021-02-26 08:07:37 +0700

    [account] Clean up

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -1,30 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { HTS_ACCOUNT_PACKAGE_NAME } from 'apis/gen/nest/hts/account/service';
-import { join } from 'path';
-import { AccountResolver } from './account.resolver';
 import { AccountService } from './account.service';
 
 @Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: HTS_ACCOUNT_PACKAGE_NAME,
-        transport: Transport.GRPC,
-        options: {
-          url: process.env.HTS_SVC_ACCOUNT,
-          package: HTS_ACCOUNT_PACKAGE_NAME,
-          protoPath: join(
-            __dirname,
-            '../../../apis/proto/hts/account/service.proto',
-          ),
-          loader: {
-            includeDirs: [join(__dirname, '../../../apis/proto')],
-          },
-        },
-      },
-    ]),
-  ],
-  providers: [AccountResolver, AccountService],
+  imports: [],
+  providers: [AccountService],
 })
 export class AccountModule {}
