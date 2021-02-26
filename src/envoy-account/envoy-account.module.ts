524a4a27bb185163a36087e13bf5d624285556f8
FleeingSunlight <gh@fleeingsunlight.dev>
2021-02-26 10:06:53 +0700

    [envoy-account] Add isAuthenticated method

--- src/envoy-account/envoy-account.module.ts
+++ src/envoy-account/envoy-account.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { EnvoyAccountService } from './envoy-account.service';
 
 @Module({
-  providers: [EnvoyAccountService]
+  providers: [EnvoyAccountService],
+  exports: [EnvoyAccountService],
 })
 export class EnvoyAccountModule {}
--- src/envoy-account/envoy-account.service.ts
+++ src/envoy-account/envoy-account.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+import {
+  AccountServiceClient,
+  ACCOUNT_SERVICE_NAME,
+  HTS_ACCOUNT_PACKAGE_NAME,
+} from 'src/apis/hts/account/service';
+import { map } from 'rxjs/operators';
 
 @Injectable()
-export class EnvoyAccountService {}
+export class EnvoyAccountService implements OnModuleInit {
+  private accountService: AccountServiceClient;
+
+  constructor(@Inject(HTS_ACCOUNT_PACKAGE_NAME) private client: ClientGrpc) {}
+
+  onModuleInit() {
+    this.accountService = this.client.getService<AccountServiceClient>(
+      ACCOUNT_SERVICE_NAME,
+    );
+  }
+
+  isAuthenticated(accessToken: string): Observable<boolean> {
+    return this.accountService
+      .isAuthenticated({ accessToken })
+      .pipe(map((result) => result.isOk));
+  }
+}
