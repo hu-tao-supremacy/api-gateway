dd7e37e89df9b102af3d8b8a31d5c29cb38df0d0
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-19 15:10:15 +0700

    fix(account): `Envoy` -> `Proxy`

--- src/account/account.resolver.ts
+++ src/account/account.resolver.ts
@@ -1,8 +1,8 @@
 import { Query, Resolver } from '@nestjs/graphql';
-import { EnvoyAccountService } from 'src/envoy-account/envoy-account.service';
+import { ProxyAccountService } from 'src/proxy-account/proxy-account.service';
 import { Account } from 'src/models/account.model';
 
 @Resolver((_) => Account)
 export class AccountResolver {
-  constructor(private readonly envoyAccountService: EnvoyAccountService) {}
+  constructor(private readonly proxyAccountService: ProxyAccountService) {}
 }
