ebd45e22f7515b454920fd052cd0660284cf39ac
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-20 17:25:31 +0700

    feat(proxy-account): add ping() to ProxyAccountService

--- src/proxy-account/proxy-account.service.ts
+++ src/proxy-account/proxy-account.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import {
+  AccountServiceClient,
+  ACCOUNT_SERVICE_NAME,
+  HTS_ACCOUNT_PACKAGE_NAME,
+} from '@internal/account/service';
+import { Result } from '@internal/common/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
 
 @Injectable()
-export class ProxyAccountService {}
+export class ProxyAccountService implements OnModuleInit {
+  private accountService: AccountServiceClient;
+
+  constructor(@Inject(HTS_ACCOUNT_PACKAGE_NAME) private client: ClientGrpc) {}
+
+  onModuleInit() {
+    this.accountService = this.client.getService<AccountServiceClient>(
+      ACCOUNT_SERVICE_NAME,
+    );
+  }
+
+  ping(): Observable<Result> {
+    return this.accountService.ping({});
+  }
+}
