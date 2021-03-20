23040490257ea3e80faea0ab75ee5537358b35f7
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-20 17:14:40 +0700

    chore: remove rewrite/

--- src/rewrite/index.ts
+++
@@ -1,8 +0,0 @@
-import * as common from '../apis/hts/common/common';
-import { StringValue } from '../apis/google/protobuf/wrappers';
-import { Weaken } from './utils';
-
-export interface User extends Weaken<common.User, 'nickname', 'chulaId'> {
-  nickname: StringValue;
-  chulaId: StringValue;
-}
--- src/rewrite/util.ts
+++
@@ -1,3 +0,0 @@
-export type Weaken<T, K extends keyof T> = {
-  [P in keyof T]: P extends K ? any : T[P];
-};
