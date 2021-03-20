7677bff7361ca6429c2b9ce329da9dd2850c1528
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-20 18:00:23 +0700

    feat(proxy-participant): add getAllEvents()

--- src/proxy-participant/proxy-participant.service.ts
+++ src/proxy-participant/proxy-participant.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Event, Result } from '@internal/common/common';
+import {
+  HTS_PARTICIPANT_PACKAGE_NAME,
+  ParticipantServiceClient,
+  PARTICIPANT_SERVICE_NAME,
+} from '@internal/participant/service';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable()
-export class ProxyParticipantService {}
+export class ProxyParticipantService implements OnModuleInit {
+  private participantService: ParticipantServiceClient;
+
+  constructor(
+    @Inject(HTS_PARTICIPANT_PACKAGE_NAME) private client: ClientGrpc,
+  ) {}
+
+  onModuleInit() {
+    this.participantService = this.client.getService<ParticipantServiceClient>(
+      PARTICIPANT_SERVICE_NAME,
+    );
+  }
+
+  ping(): Observable<Result> {
+    return this.participantService.ping({});
+  }
+
+  getAllEvents(): Observable<Event[]> {
+    return this.participantService
+      .getAllEvents({})
+      .pipe(map((data) => data.event));
+  }
+}
