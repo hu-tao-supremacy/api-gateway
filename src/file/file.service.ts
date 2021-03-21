2fc3a68f96ca0aaf07e83a4b021f7de7a6116c96
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-21 17:25:02 +0700

    feat(file): add `@google-cloud/storage`

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Storage } from '@google-cloud/storage';
+import { Readable } from 'stream';
 
 @Injectable()
-export class FileService {}
+export class FileService {
+  private cloudStorage = new Storage().bucket();
+
+  async upload(filePath: string, readableStream: Readable) {
+    const file = this.cloudStorage.file(filePath);
+    await new Promise((resolve, reject) =>
+      readableStream
+        .pipe(file.createWriteStream())
+        .on('error', reject)
+        .on('finish', resolve),
+    );
+  }
+}
