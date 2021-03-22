d719156dd161330bea5142d87558334882fcd023
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-22 17:37:14 +0700

    feat(auth): generate AuthResolver

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthResolver } from './auth.resolver';
 
 @Module({
-  providers: [AuthService],
+  providers: [AuthService, AuthResolver],
   exports: [AuthService],
 })
 export class AuthModule {}
