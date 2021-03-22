7bb03bfaad4a0012c31b84f2720b8cffcb1ef844
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-22 18:27:02 +0700

    feat(proxy-facility): connect to FacilityService

--- src/proxy-facility/proxy-facility.service.ts
+++ src/proxy-facility/proxy-facility.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import {
+  FacilityServiceClient,
+  FACILITY_SERVICE_NAME,
+  HTS_FACILITY_PACKAGE_NAME,
+} from '@internal/facility/service';
+import { ClientGrpc } from '@nestjs/microservices';
+import { Result } from '@internal/common/common';
+import { Observable } from 'rxjs';
 
 @Injectable()
-export class ProxyFacilityService {}
+export class ProxyFacilityService implements OnModuleInit {
+  private facilityService: FacilityServiceClient;
+
+  constructor(@Inject(HTS_FACILITY_PACKAGE_NAME) private client: ClientGrpc) {}
+
+  onModuleInit() {
+    this.facilityService = this.client.getService<FacilityServiceClient>(
+      FACILITY_SERVICE_NAME,
+    );
+  }
+
+  ping(): Observable<Result> {
+    return this.facilityService.ping();
+  }
+}
