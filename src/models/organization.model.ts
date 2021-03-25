1f4a2fe3455f5ff66669f6fb9b67bf5810634cdd
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-25 13:51:29 +0700

    feat(models/org): add additional required fields

--- src/models/organization.model.ts
+++ src/models/organization.model.ts
@@ -11,4 +11,46 @@ export class Organization implements IOrganization {
 
   @Field((_) => Boolean)
   isVerified: boolean;
+
+  @Field({ nullable: true })
+  abbreviation: string | undefined;
+
+  @Field({ nullable: true })
+  advisor: string | undefined;
+
+  @Field({ nullable: true })
+  associatedFaculty: string | undefined;
+
+  @Field({ nullable: true })
+  description: string | undefined;
+
+  @Field({ nullable: true })
+  facebookPage: string | undefined;
+
+  @Field({ nullable: true })
+  instagram: string | undefined;
+
+  @Field({ nullable: true })
+  lineOfficialAccount: string | undefined;
+
+  @Field({ nullable: true })
+  email: string | undefined;
+
+  @Field({ nullable: true })
+  contactFullName: string | undefined;
+
+  @Field({ nullable: true })
+  contactEmail: string | undefined;
+
+  @Field({ nullable: true })
+  contactPhoneNumber: string | undefined;
+
+  @Field({ nullable: true })
+  contactLineId: string | undefined;
+
+  @Field({ nullable: true })
+  profilePictureUrl: string | undefined;
+
+  @Field({ nullable: true })
+  profilePictureHash: string | undefined;
 }
