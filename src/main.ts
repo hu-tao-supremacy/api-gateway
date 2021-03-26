99b736c1575859c707887b6fcceac496f4e233a3
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-27 00:18:16 +0700

    fix(main.ts): process.env.PORT || 5000

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { Gender, Status, DayOfWeek, Permission } from '@internal/common/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(5000);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
