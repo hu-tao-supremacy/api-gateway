00fbc14bcefad40f1c056881b5b76cce5a5032e2
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-27 15:40:51 +0700

    feat(models): add fromInternal() to Organization model

--- src/models/organization.model.ts
+++ src/models/organization.model.ts
@@ -1,5 +1,6 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql';
 import { Organization as IOrganization } from '@gql/common/common';
+import { Organization as OrganizationInput } from '@internal/common/common';
 
 @ObjectType()
 export class Organization implements IOrganization {
@@ -53,4 +54,26 @@ export class Organization implements IOrganization {
 
   @Field({ nullable: true })
   profilePictureHash: string | undefined;
+
+  fromInternal(_org: OrganizationInput): Organization {
+    const org = new Organization();
+    org.id = _org.id;
+    org.name = _org.name;
+    org.isVerified = _org.isVerified;
+    org.abbreviation = _org.abbreviation?.value;
+    org.advisor = _org.advisor?.value;
+    org.associatedFaculty = _org.associatedFaculty?.value;
+    org.description = _org.description?.value;
+    org.facebookPage = _org.facebookPage?.value;
+    org.instagram = _org.instagram?.value;
+    org.lineOfficialAccount = _org.lineOfficialAccount?.value;
+    org.email = _org.email?.value;
+    org.contactFullName = _org.contactFullName?.value;
+    org.contactEmail = _org.contactEmail?.value;
+    org.contactPhoneNumber = _org.contactPhoneNumber?.value;
+    org.contactLineId = _org.contactLineId?.value;
+    org.profilePictureUrl = _org.profilePictureUrl?.value;
+    org.profilePictureHash = _org.profilePictureHash?.value;
+    return org;
+  }
 }
