994455721961f249f8106bd44af88f1ea7355cec
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-27 17:51:58 +0700

    feat(models): add from(_:LocationDTO) to Location model

--- src/models/location.model.ts
+++ src/models/location.model.ts
@@ -1,5 +1,6 @@
 import { Location as ILocation } from '@gql/common/common';
 import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { Location as LocationDTO } from '@internal/common/common';
 
 @ObjectType()
 export class Location implements ILocation {
@@ -10,7 +11,7 @@ export class Location implements ILocation {
   name: string;
 
   @Field({ nullable: true })
-  description: string;
+  description: string | undefined;
 
   @Field()
   googleMapUrl: string;
@@ -20,4 +21,17 @@ export class Location implements ILocation {
 
   @Field({ nullable: true })
   travelInformationImageHash: string | undefined;
+
+  static from(_location: LocationDTO): Location {
+    const location = new Location();
+    location.id = Number(_location.id.toString());
+    location.name = _location.name;
+    location.description = _location.description?.value;
+    location.googleMapUrl = _location.googleMapUrl;
+    location.travelInformationImageUrl =
+      _location.travelInformationImageUrl?.value;
+    location.travelInformationImageHash =
+      _location.travelInformationImageHash?.value;
+    return location;
+  }
 }
