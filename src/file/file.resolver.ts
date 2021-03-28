f4aa977181c3ecf159d8b314bdebbaf488678b9e
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-28 23:09:05 +0700

    feat(file): add `upload` mutation

--- src/file/file.resolver.ts
+++ src/file/file.resolver.ts
@@ -1,4 +1,21 @@
-import { Resolver } from '@nestjs/graphql';
+import { FileUploadInput } from '@inputs/file.input';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { from, Observable } from 'rxjs';
+import { FileService } from './file.service';
 
 @Resolver()
-export class FileResolver {}
+export class FileResolver {
+  constructor(private readonly fileService: FileService) {}
+  @Mutation(() => Boolean)
+  upload(@Args('input') input: FileUploadInput): Observable<boolean> {
+    return from(
+      new Promise<boolean>(async (resolve, reject) =>
+        input.file
+          .createReadStream()
+          .pipe(this.fileService.createWriteStream(input.file.filename))
+          .on('finish', () => resolve(true))
+          .on('error', () => reject(false)),
+      ),
+    );
+  }
+}
