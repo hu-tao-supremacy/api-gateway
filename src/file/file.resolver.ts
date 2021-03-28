34f5c1c0ff8e1909bfd0e1dc592b32c6ced4029f
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-28 23:09:54 +0700

    feat(file): decorate FileResolver with UseGuards(AuthGuard)

--- src/file/file.resolver.ts
+++ src/file/file.resolver.ts
@@ -1,11 +1,15 @@
 import { FileUploadInput } from '@inputs/file.input';
+import { UseGuards } from '@nestjs/common';
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { from, Observable } from 'rxjs';
+import { AuthGuard } from 'src/guards/auth.guard';
 import { FileService } from './file.service';
 
+@UseGuards(AuthGuard)
 @Resolver()
 export class FileResolver {
   constructor(private readonly fileService: FileService) {}
+
   @Mutation(() => Boolean)
   upload(@Args('input') input: FileUploadInput): Observable<boolean> {
     return from(
