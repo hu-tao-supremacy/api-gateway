d654ab7e8693893cf2024d8e2c1aafb3783b549f
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-28 19:28:11 +0700

    feat(models/account): add from()

--- src/models/account.model.ts
+++ src/models/account.model.ts
@@ -1,5 +1,6 @@
 import { Field, Int, ObjectType, registerEnumType } from '@nestjs/graphql';
 import { User, Gender } from '@gql/common/common';
+import { User as UserDTO } from '@internal/common/common';
 
 registerEnumType(Gender, { name: 'Gender' });
 
@@ -34,4 +35,19 @@ export class Account implements User {
 
   @Field((type) => Gender)
   gender: Gender;
+
+  static from(_user: UserDTO): Account {
+    const account = new Account();
+    account.id = _user.id;
+    account.gender = _user.gender;
+    account.profilePictureUrl = _user.profilePictureUrl?.value;
+    account.address = _user.address?.value;
+    account.chulaId = _user.chulaId?.value;
+    account.isChulaStudent = _user.isChulaStudent;
+    account.nickname = _user.nickname?.value;
+    account.email = _user.email;
+    account.lastName = _user.lastName;
+    account.firstName = _user.firstName;
+    return account;
+  }
 }
