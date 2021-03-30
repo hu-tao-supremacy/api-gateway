c98edcfbbda2b0cda1f280b1fcc32c94283df2c6
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-30 23:04:51 +0700

    feat(org): add placeholder methods to OrganizationResolver

--- src/organization/organization.module.ts
+++ src/organization/organization.module.ts
@@ -1,4 +1,9 @@
+import { ProxyOrganizerModule } from '@hu-tao-supremacy:organizer/proxy-organizer.module';
 import { Module } from '@nestjs/common';
+import { OrganizationResolver } from './organization.resolver';
 
-@Module({})
-export class OrganizationModule {}
+@Module({
+    imports: [ProxyOrganizerModule],
+    providers: [OrganizationResolver]
+})
+export class OrganizationModule { }
--- src/organization/organization.resolver.ts
+++ src/organization/organization.resolver.ts
@@ -1,4 +1,22 @@
-import { Resolver } from '@nestjs/graphql';
+import { Organization } from '@entities/organization.entity';
+import { ProxyOrganizerService } from '@hu-tao-supremacy:organizer/proxy-organizer.service';
+import { ProxyParticipantService } from '@hu-tao-supremacy:participant/proxy-participant.service';
+import { Args, Int, Query, Resolver } from '@nestjs/graphql';
 
-@Resolver()
-export class OrganizationResolver {}
+@Resolver((_) => Organization)
+export class OrganizationResolver {
+    constructor(
+        private readonly proxyOrganizerService: ProxyOrganizerService,
+        private readonly proxyParticipantService: ProxyParticipantService
+    ) { }
+
+    @Query((_) => [Organization])
+    organizations() {
+        return []
+    }
+
+    @Query((_) => Organization)
+    organization(@Args('id', { type: () => Int }) id: number) {
+        return null;
+    }
+}
