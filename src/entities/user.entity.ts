a9f1c252f3fe243a45fbd208b8561380c9cfee81
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-30 15:39:16 +0700

    feat(entities/user): add GraphQL decorators

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,35 +1,48 @@
+import { Gender } from "@gql/common/common";
+import { Field, Int, ObjectType } from "@nestjs/graphql";
 import { PrimaryGeneratedColumn, Column, Entity, Index } from "typeorm";
 
+@ObjectType()
 @Entity()
 export class User {
+  @Field(_ => Int)
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field()
   @Column()
   firstName: string;
 
+  @Field()
   @Column()
   lastName: string;
 
+  @Field()
   @Column({ unique: true })
   email: string;
 
+  @Field({ nullable: true })
   @Column({ nullable: true })
   nickname?: string;
 
+  @Field({ nullable: true })
   @Index({ unique: true, where: "chula_id IS NOT NULL" })
   @Column({ nullable: true })
   chulaId?: string;
 
+  @Field({ nullable: true })
   @Column({ nullable: true })
   address?: string;
 
+  @Field({ nullable: true })
   @Column({ nullable: true })
   profilePictureUrl?: string;
 
+  @Field(_ => Boolean)
   @Column()
   isChulaStudent: boolean;
 
+  @Field(_ => Gender)
   @Column("enum", { enum: ["M", "F", "NS"] })
   gender: string;
 }
