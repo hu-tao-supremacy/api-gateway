2f5e4ba17e17d9abeb949fffa377ee24c184f12c
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-30 14:26:30 +0700

    feat(entities/facility-request): add GraphQL decorators

--- src/entities/facility-request.entity.ts
+++ src/entities/facility-request.entity.ts
@@ -1,33 +1,45 @@
+import { Field, Int, ObjectType } from "@nestjs/graphql";
+import { Status } from "@gql/common/common";
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { Event } from "./event.entity";
 import { Facility } from "./facility.entity";
 
+@ObjectType()
 @Entity()
 export class FacilityRequest {
+  @Field(_ => Int)
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field(_ => Int)
   @Column()
   eventId: number;
 
+  @Field(_ => Event)
   @ManyToOne(() => Event)
   event: Event;
 
+  @Field(_ => Int)
   @Column()
   facilityId: number;
 
+  @Field(_ => Facility)
   @ManyToOne(() => Facility, { onDelete: "CASCADE" })
   facility: Facility;
 
+  @Field(_ => Status)
   @Column("enum", { enum: ["PENDING", "APPROVED", "REJECTED"] })
   status: string;
 
+  @Field({ nullable: true })
   @Column({ nullable: true })
   rejectReason?: string;
 
+  @Field({ nullable: true })
   @Column("timestamptz", { nullable: true })
   start: string;
 
+  @Field({ nullable: true })
   @Column("timestamptz", { nullable: true })
   finish: string;
 }
