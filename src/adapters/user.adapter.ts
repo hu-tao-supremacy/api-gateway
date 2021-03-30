06f8a5300851b924c8a22b5b866342fabb1ceba3
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-30 12:27:51 +0700

    feat(adapters): add UserAdapter

---
+++ src/adapters/user.adapter.ts
@@ -0,0 +1,7 @@
+import { BaseAdapter } from '@adapters/base.adapter';
+import { User as UserIF } from '@interchange-formats/common/common';
+import { User as User } from '@entities/user.entity';
+
+export class UserAdapter extends BaseAdapter<UserIF, User> {
+  optionalFields: ['chulaId', 'address', 'profilePictureUrl', 'nickname'];
+}
