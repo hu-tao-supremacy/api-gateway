e42f7996e0285f55e19eb2e862df0996e35ff9c2
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-30 15:37:44 +0700

    feat(entities/user-permission): add GraphQL decorators

--- src/entities/user-permission.entity.ts
+++ src/entities/user-permission.entity.ts
@@ -1,3 +1,4 @@
+import { Field, Int, ObjectType } from "@nestjs/graphql";
 import {
   Column,
   Entity,
@@ -7,18 +8,23 @@ import {
 } from "typeorm";
 import { UserOrganization } from "./user-organization.entity";
 
+@ObjectType()
 @Index(["userOrganizationId", "permissionName"], { unique: true })
 @Entity()
 export class UserPermission {
+  @Field(_ => Int)
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field(_ => Int)
   @Column()
   userOrganizationId: number;
 
+  @Field(_ => UserOrganization)
   @ManyToOne(() => UserOrganization, { onDelete: "CASCADE" })
   userOrganization: UserOrganization;
 
+  @Field()
   @Column()
   permissionName: string;
 }
