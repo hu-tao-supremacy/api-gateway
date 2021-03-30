bfd5b5765df7835e65b3fa237baea65487c2f2b3
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-30 15:31:16 +0700

    feat(entities/user-interest): add GraphQL decorators

--- src/entities/user-interest.entity.ts
+++ src/entities/user-interest.entity.ts
@@ -1,3 +1,4 @@
+import { Field, Int, ObjectType } from "@nestjs/graphql";
 import {
   Column,
   Entity,
@@ -8,21 +9,27 @@ import {
 import { Tag } from "./tag.entity";
 import { User } from "./user.entity";
 
+@ObjectType()
 @Index(["userId", "tagId"], { unique: true })
 @Entity()
 export class UserInterest {
+  @Field(_ => Int)
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field(_ => Int)
   @Column()
   userId: number;
 
+  @Field(_ => User)
   @ManyToOne(() => User, { onDelete: "CASCADE" })
   user: User;
 
+  @Field(_ => Int)
   @Column()
   tagId: number;
 
+  @Field(_ => Tag)
   @ManyToOne(() => Tag, { onDelete: "CASCADE" })
   tag: Tag;
 }
