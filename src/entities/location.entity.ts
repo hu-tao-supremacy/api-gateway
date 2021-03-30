5c3e70392435a32413b379d12a7e093038d72dee
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-30 14:34:15 +0700

    feat(entities/location): add GraphQL decorators

--- src/entities/location.entity.ts
+++ src/entities/location.entity.ts
@@ -1,22 +1,30 @@
+import { Field, Int, ObjectType } from "@nestjs/graphql";
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+@ObjectType()
 @Entity()
 export class Location {
+  @Field(_ => Int)
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field()
   @Column()
   name: string;
 
+  @Field()
   @Column()
   googleMapUrl: string;
 
+  @Field({ nullable: true })
   @Column({ nullable: true })
   description?: string;
 
+  @Field({ nullable: true })
   @Column({ nullable: true })
   travelInformationImageUrl?: string;
 
+  @Field({ nullable: true })
   @Column({ nullable: true })
   travelInformationImageHash?: string;
 }
