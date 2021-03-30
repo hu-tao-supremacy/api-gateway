fdbc874dcaa4143da888a40cc62522035df760f7
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-30 12:48:55 +0700

    feat(entities): decorate Event entity with GraphQL related decorators

--- src/entities/event.entity.ts
+++ src/entities/event.entity.ts
@@ -1,3 +1,4 @@
+import { ObjectType, Field, Int } from '@nestjs/graphql';
 import {
   Column,
   Entity,
@@ -7,50 +8,62 @@ import {
   OneToMany,
   OneToOne,
   PrimaryGeneratedColumn,
-} from "typeorm";
-import { Location } from "./location.entity";
-import { Organization } from "./organization.entity";
-import { QuestionGroup } from "./question-group.entity";
+} from 'typeorm';
+import { Location } from './location.entity';
+import { Organization } from './organization.entity';
+import { QuestionGroup } from './question-group.entity';
 
-@Index(["organizationId", "name"], { unique: true })
+@ObjectType()
+@Index(['organizationId', 'name'], { unique: true })
 @Entity()
 export class Event {
+  @Field((_) => Int)
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field((_) => Int)
   @Column()
   organizationId: number;
 
-  @ManyToOne(() => Organization, { onDelete: "CASCADE" })
+  @ManyToOne(() => Organization, { onDelete: 'CASCADE' })
   organization: Organization;
 
+  @Field((_) => Int, { nullable: true })
   @Column({ nullable: true })
   locationId?: number;
 
   @ManyToOne(() => Location)
   location?: Location;
 
+  @Field()
   @Column()
   description: string;
 
+  @Field()
   @Column()
   name: string;
 
+  @Field({ nullable: true })
   @Column({ nullable: true })
   coverImageUrl?: string;
 
+  @Field({ nullable: true })
   @Column({ nullable: true })
   coverImageHash?: string;
 
+  @Field({ nullable: true })
   @Column({ nullable: true })
   posterImageUrl?: string;
 
+  @Field({ nullable: true })
   @Column({ nullable: true })
   posterImageHash?: string;
 
+  @Field({ nullable: true })
   @Column({ nullable: true })
   profileImageUrl?: string;
 
+  @Field({ nullable: true })
   @Column({ nullable: true })
   profileImageHash?: string;
 
