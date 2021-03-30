76ed4be71bacd8b93c31656c65ca14a216cf56c6
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-30 18:00:04 +0700

    feat(adapters/base): handle google.protobuf.Timestamp -> Date()

--- src/adapters/base.adapter.ts
+++ src/adapters/base.adapter.ts
@@ -1,8 +1,9 @@
-import { mapKeys, mapValues } from 'lodash';
+import { has, keys, mapValues } from 'lodash';
 import { isLong } from 'long';
+import { DateTime } from 'luxon';
 
 export class BaseAdapter<IF extends object, E extends object> {
-  optionalFields: string[];
+  optionalFields: string[] = [];
 
   wrapperToOptional(field: any) {
     return field?.value;
@@ -23,6 +24,16 @@ export class BaseAdapter<IF extends object, E extends object> {
       if (isLong(a)) {
         // @ts-ignore
         a = Number(a.toString());
+      }
+
+      // google.protobuf.Timestamp
+      if (has(a, 'seconds') && has(a, 'nanos') && keys(a).length === 2) {
+        // @ts-ignore
+        a.seconds = isLong(a.seconds) ? Number(a.seconds.toString()) : a.seconds
+        // @ts-ignore
+        a.nanos = isLong(a.nanos) ? Number(a.nanos.toString()) : a.nanos
+        // @ts-ignore
+        a = DateTime.fromJSDate(new Date(a.seconds * 1000 + a.nanos / 1000)).toISO()
       }
 
       return a;
