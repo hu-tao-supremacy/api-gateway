8b231e747afe87615ca7950fc2432cc6af48814f
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-30 14:30:05 +0700

    feat(entities/facility): add GraphQL decorators

--- src/entities/facility.entity.ts
+++ src/entities/facility.entity.ts
@@ -1,29 +1,39 @@
+import { Field, Float, Int, ObjectType } from "@nestjs/graphql";
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { Organization } from "./organization.entity";
 
+@ObjectType()
 @Entity()
 export class Facility {
+  @Field(_ => Int)
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field(_ => Int)
   @Column()
   organizationId: number;
 
+  @Field(_ => Organization)
   @ManyToOne(() => Organization, { onDelete: "CASCADE" })
   organization: Organization;
 
+  @Field()
   @Column({ unique: true })
   name: string;
 
+  @Field(_ => Float)
   @Column()
   latitude: number;
 
+  @Field(_ => Float)
   @Column()
   longitude: number;
 
+  @Field() 
   @Column("json")
   operatingHours: string;
 
+  @Field() 
   @Column()
   description: string;
 }
