4a015741148f71d039ab4d7b9c7fc0830b2a484c
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-30 23:11:52 +0700

    fix(org): add ProxyParticipantModule to `imports`

--- src/organization/organization.module.ts
+++ src/organization/organization.module.ts
@@ -1,9 +1,10 @@
 import { ProxyOrganizerModule } from '@hu-tao-supremacy:organizer/proxy-organizer.module';
+import { ProxyParticipantModule } from '@hu-tao-supremacy:participant/proxy-participant.module';
 import { Module } from '@nestjs/common';
 import { OrganizationResolver } from './organization.resolver';
 
 @Module({
-    imports: [ProxyOrganizerModule],
+    imports: [ProxyOrganizerModule, ProxyParticipantModule],
     providers: [OrganizationResolver]
 })
 export class OrganizationModule { }
