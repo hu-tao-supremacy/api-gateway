8da1dfb8cb8060cd361dc85dfe9723274ced64dd
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-30 15:32:12 +0700

    feat(entities/user-org): add GraphQL decorators

--- src/entities/user-organization.entity.ts
+++ src/entities/user-organization.entity.ts
@@ -1,3 +1,4 @@
+import { Field, Int, ObjectType } from "@nestjs/graphql";
 import {
   Column,
   Entity,
@@ -8,21 +9,27 @@ import {
 import { Organization } from "./organization.entity";
 import { User } from "./user.entity";
 
+@ObjectType()
 @Index(["userId", "organizationId"], { unique: true })
 @Entity()
 export class UserOrganization {
+  @Field(_ => Int)
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field(_ => Int)
   @Column()
   userId: number;
 
+  @Field(_ => User)
   @ManyToOne(() => User, { onDelete: "CASCADE" })
   user: User;
 
+  @Field(_ => Int)
   @Column()
   organizationId: number;
 
+  @Field(_ => Organization)
   @ManyToOne(() => Organization, { onDelete: "CASCADE" })
   organization: Organization;
 }
