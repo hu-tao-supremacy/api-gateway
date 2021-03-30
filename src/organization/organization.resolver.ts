5d9d41037057cd7c0de351a5f19152d10f9b22cd
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-30 23:18:46 +0700

    feat(org): add `events` field resolver

--- src/organization/organization.resolver.ts
+++ src/organization/organization.resolver.ts
@@ -1,7 +1,8 @@
+import { Event } from '@entities/event.entity';
 import { Organization } from '@entities/organization.entity';
 import { ProxyOrganizerService } from '@hu-tao-supremacy:organizer/proxy-organizer.service';
 import { ProxyParticipantService } from '@hu-tao-supremacy:participant/proxy-participant.service';
-import { Args, Int, Query, Resolver } from '@nestjs/graphql';
+import { Args, Int, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
 
 @Resolver((_) => Organization)
 export class OrganizationResolver {
@@ -18,5 +19,10 @@ export class OrganizationResolver {
     @Query((_) => Organization)
     organization(@Args('id', { type: () => Int }) id: number) {
         return this.proxyOrganizerService.getOrganizationById(id);
+    }
+
+    @ResolveField((_) => [Event])
+    events(@Parent() org: Organization) {
+        return this.proxyParticipantService.getEventsByOrganizationId(org.id);
     }
 }
