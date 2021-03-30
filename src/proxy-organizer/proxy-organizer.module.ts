230c96266192a72b0646bc46e7d18857f3c94341
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-30 23:20:47 +0700

    chore(proxy-organizer): add ProxyOrganizerService to `exports`

--- src/proxy-organizer/proxy-organizer.module.ts
+++ src/proxy-organizer/proxy-organizer.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ProxyOrganizerService } from './proxy-organizer.service';
 
 @Module({
-  providers: [ProxyOrganizerService]
+  providers: [ProxyOrganizerService],
+  exports: [ProxyOrganizerService],
 })
 export class ProxyOrganizerModule {}
