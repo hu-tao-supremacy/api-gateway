d64ae14464df68e2002c1add2ee29f7f064fc173
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-30 15:29:54 +0700

    feat(entities/user-event): add GraphQL decorators

--- src/entities/user-event.entity.ts
+++ src/entities/user-event.entity.ts
@@ -1,3 +1,5 @@
+import { Status } from "@gql/common/common";
+import { Field, Int, ObjectType } from "@nestjs/graphql";
 import {
   Column,
   Entity,
@@ -8,31 +10,40 @@ import {
 import { Event } from "./event.entity";
 import { User } from "./user.entity";
 
+@ObjectType()
 @Index(["userId", "eventId"], { unique: true })
 @Index(["eventId", "ticket"], { unique: true })
 @Entity()
 export class UserEvent {
+  @Field(_ => Int)
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field(_ => Int)
   @Column()
   userId: number;
 
+  @Field(_ => User)
   @ManyToOne(() => User, { onDelete: "CASCADE" })
   user: User;
 
+  @Field(_ => Int) 
   @Column()
   eventId: number;
 
+  @Field(_ => Event)
   @ManyToOne(() => Event, { onDelete: "CASCADE" })
   event: Event;
 
+  @Field(_ => Int, { nullable: true }) 
   @Column({ nullable: true })
   rating?: number;
 
+  @Field({ nullable: true })
   @Column({ nullable: true })
   ticket?: string;
 
+  @Field(_ => Status)
   @Column("enum", { enum: ["PENDING", "APPROVED", "REJECTED"] })
   status: string;
 }
