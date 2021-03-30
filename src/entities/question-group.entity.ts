e0d8a98c3da59d28d68b3c5f66278a4c28bc7e6d
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-30 15:27:12 +0700

    feat(entities/question-group): add GraphQL decorators

--- src/entities/question-group.entity.ts
+++ src/entities/question-group.entity.ts
@@ -1,3 +1,5 @@
+import { Field, Int, ObjectType } from "@nestjs/graphql";
+import { QuestionGroupType } from "@gql/common/common";
 import {
   Column,
   Entity,
@@ -9,27 +11,35 @@ import {
 import { Event } from "./event.entity";
 import { Question } from "./question.entity";
 
+@ObjectType()
 @Index(["eventId", "type", "seq"], { unique: true })
 @Entity()
 export class QuestionGroup {
+  @Field(_ => Int)
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field(_ => Int)
   @Column()
   eventId: number;
 
+  @Field(_ => Event)
   @ManyToOne(() => Event, { onDelete: "CASCADE" })
   event: Event;
 
+  @Field(_ => QuestionGroupType)
   @Column("enum", { enum: ["PRE_EVENT", "POST_EVENT"] })
   type: string;
 
+  @Field(_ => Int)
   @Column()
   seq: number;
 
+  @Field()
   @Column()
   title: string;
 
+  @Field(_ => [Question])
   @OneToMany(() => Question, (question) => question.questionGroup)
   questions: Question[];
 }
