893112cc186a7fad625d3728a31153d5656657d4
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-30 23:10:51 +0700

    fix(inputs/org): import InputType from nestjs/graphql instead of nestjs/microservices

--- src/inputs/organization.input.ts
+++ src/inputs/organization.input.ts
@@ -1,4 +1,4 @@
-import { InputType } from '@nestjs/microservices';
+import { InputType } from '@nestjs/graphql';
 
 @InputType()
-export class CreateOrganizationInput {}
+export class CreateOrganizationInput { }
