200c552c117a29c65e444f01cea2bdbbda7165c7
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-30 15:27:54 +0700

    feat(entities/tag): add GraphQL decorators

--- src/entities/tag.entity.ts
+++ src/entities/tag.entity.ts
@@ -1,10 +1,14 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
 
+@ObjectType()
 @Entity()
 export class Tag {
+  @Field(_ => Int)
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field()
   @Column({ unique: true })
   name: string;
 }
