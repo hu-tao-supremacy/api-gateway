d3184f42e6ef145c918810309340471fef59d016
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-30 16:21:49 +0700

    feat(event): add `durations` field resolver

--- src/event/event.resolver.ts
+++ src/event/event.resolver.ts
@@ -1,7 +1,6 @@
 import { Field, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
 import { ProxyParticipantService } from 'src/proxy-participant/proxy-participant.service';
-import { Event } from 'src/models/event.model';
-import { Organization } from 'src/models/organization.model';
+import { Event } from '@entities/event.entity';
 import { EventService } from './event.service';
 import { map } from 'rxjs/operators';
 import { ProxyOrganizerService } from 'src/proxy-organizer/proxy-organizer.service';
@@ -40,5 +39,11 @@ export class EventResolver {
   tags(@Parent() event: Event) {
     const { id } = event;
     return this.proxyParticipantService.getEventTags(id);
+  }
+
+  @ResolveField()
+  durations(@Parent() event: Event) {
+    const { id } = event;
+    return this.proxyParticipantService.getEventDurationsByEventId(id);
   }
 }
