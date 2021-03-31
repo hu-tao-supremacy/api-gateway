971360284ac134f283cacb595524227c710e0133
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-31 17:58:31 +0700

    feat(org): add createOrg() mutation

--- src/organization/organization.resolver.ts
+++ src/organization/organization.resolver.ts
@@ -1,8 +1,14 @@
 import { Event } from '@entities/event.entity';
 import { Organization } from '@entities/organization.entity';
+import { User } from '@entities/user.entity';
 import { ProxyOrganizerService } from '@hu-tao-supremacy:organizer/proxy-organizer.service';
 import { ProxyParticipantService } from '@hu-tao-supremacy:participant/proxy-participant.service';
-import { Args, Int, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import { CreateOrganizationInput } from '@inputs/organization.input';
+import { UseGuards } from '@nestjs/common';
+import { Args, Int, Mutation, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import { merge } from 'lodash';
+import { CurrentUser } from 'src/decorators/user.decorator';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Resolver((_) => Organization)
 export class OrganizationResolver {
@@ -24,5 +30,13 @@ export class OrganizationResolver {
     @ResolveField((_) => [Event])
     events(@Parent() org: Organization) {
         return this.proxyParticipantService.getEventsByOrganizationId(org.id);
+    }
+
+    @UseGuards(AuthGuard)
+    @Mutation(_ => Boolean)
+    createOrganization(@CurrentUser() currentUser: User, @Args('input') input: CreateOrganizationInput) {
+        const org = new Organization()
+        merge(org, input)
+        return this.proxyOrganizerService.createOrganization(currentUser.id, org)
     }
 }
