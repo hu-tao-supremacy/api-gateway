1efa84db6387e0044911285b2ab630376d7dabd9
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-31 10:44:13 +0700

    feat(middlewares/auth): implement AuthMiddleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,8 +1,36 @@
+import { User } from '@entities/user.entity';
+import { ProxyAccountService } from '@hu-tao-supremacy:account/proxy-account.service';
 import { Injectable, NestMiddleware } from '@nestjs/common';
+import { AuthService } from 'src/auth/auth.service';
+import { Request } from 'express'
+import Base64 from 'crypto-js/enc-base64'
+import { AccessTokenPayload } from '@gql/account/service';
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: User;
+    }
+  }
+}
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
-  use(req: any, res: any, next: () => void) {
+  constructor(private readonly authService: AuthService, private readonly proxyAccountService: ProxyAccountService) { }
+
+  async use(req: Request, res: any, next: () => void) {
+    try {
+      const accessToken = req.headers.authorization?.split('Bearer ')[1];
+      if (accessToken) {
+        const isAuthenticated = await this.authService.isAuthenticated(accessToken).toPromise();
+        if (isAuthenticated) {
+          const encodedPayload = accessToken.split('.')[1]
+          const payload = JSON.parse(Base64.parse(encodedPayload).toString()) as AccessTokenPayload
+          const user = await this.proxyAccountService.getUserById(payload.userId).toPromise();
+          req.user = user
+        }
+      }
+    } catch (_) { }
     next();
   }
 }
