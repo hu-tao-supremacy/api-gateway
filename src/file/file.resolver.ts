07e4e37218d42b7a478c20a3165d1a42047f9f92
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-31 11:26:46 +0700

    feat(file): add CurrentUser decorator to `upload` mutation

--- src/file/file.resolver.ts
+++ src/file/file.resolver.ts
@@ -1,17 +1,20 @@
+import { User } from '@entities/user.entity';
 import { FileUploadInput } from '@inputs/file.input';
 import { UseGuards } from '@nestjs/common';
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { from, Observable } from 'rxjs';
+import { CurrentUser } from 'src/decorators/user.decorator';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { FileService } from './file.service';
 
 @Resolver()
 @UseGuards(AuthGuard)
 export class FileResolver {
-  constructor(private readonly fileService: FileService) {}
+  constructor(private readonly fileService: FileService) { }
 
   @Mutation(() => Boolean)
-  async upload(@Args('input') input: FileUploadInput): Promise<boolean> {
+  async upload(@CurrentUser() currentUser: User, @Args('input') input: FileUploadInput): Promise<boolean> {
+    console.log(currentUser.id)
     const { filename, createReadStream } = await input.file;
     console.log(filename);
     return new Promise<boolean>(async (resolve, reject) =>
