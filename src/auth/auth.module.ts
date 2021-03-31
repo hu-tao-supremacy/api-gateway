7566909a3832517d9f336d9d6a96aff34b299be4
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-31 17:59:16 +0700

    feat(auth): make AuthModule global module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,13 @@
-import { HttpModule, Module } from '@nestjs/common';
+import { Global, HttpModule, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
 import { ProxyAccountModule } from 'src/proxy-account/proxy-account.module';
 import { ProxyAccountService } from 'src/proxy-account/proxy-account.service';
 
+@Global()
 @Module({
   imports: [HttpModule, ProxyAccountModule],
   providers: [AuthService, ProxyAccountService, AuthResolver],
   exports: [AuthService],
 })
-export class AuthModule {}
+export class AuthModule { }
