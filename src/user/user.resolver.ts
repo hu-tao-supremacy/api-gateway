7317220fbd0036d5f693cbadfaf1fd3b7adae03f
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-04 00:15:44 +0700

    feat(user): add currentUser query

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,4 +1,14 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
+import { User } from '@entities/user.entity';
+import { NotImplementedException, UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { CurrentUser } from 'src/decorators/user.decorator';
 
-@Resolver()
-export class UserResolver {}
+@Resolver(() => User)
+export class UserResolver {
+  @UseGuards(AuthGuard)
+  @Query(() => User)
+  currentUser(@CurrentUser() user: User) {
+    throw new NotImplementedException();
+  }
+}
