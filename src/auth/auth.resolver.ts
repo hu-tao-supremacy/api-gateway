29f3c8800eb447a360f7dfd48c306cd24e4cae48
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-04 19:27:02 +0700

    feat(auth): add signInWithServiceAccount() mutation for development testing

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -2,6 +2,7 @@ import { AuthenticateInput } from '@inputs/auth.input';
 import { Resolver, Mutation, Args } from '@nestjs/graphql';
 import { AuthenticateOutput } from './auth.model';
 import { AuthService } from './auth.service';
+import { decode } from 'js-base64';
 
 @Resolver()
 export class AuthResolver {
@@ -10,5 +11,11 @@ export class AuthResolver {
   @Mutation(() => AuthenticateOutput)
   authenticate(@Args('input') input: AuthenticateInput) {
     return this.service.authenticate(input.providerAccessToken);
+  }
+
+  @Mutation(() => AuthenticateOutput)
+  signInWithServiceAccount(@Args('serviceAccount') input: string) {
+    const serviceAccount = JSON.parse(decode(input)) as any;
+    return this.service.signInWithServiceAccount(serviceAccount);
   }
 }
