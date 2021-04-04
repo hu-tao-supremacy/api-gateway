99bde4780f16d2ec3e96cae9c3c3e116beda1195
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-04 21:02:45 +0700

    feat(inputs/user): implement UpdateUserInput

--- src/inputs/user.input.ts
+++ src/inputs/user.input.ts
@@ -1,4 +1,9 @@
-import { InputType } from '@nestjs/graphql';
+import { User } from '@entities/user.entity';
+import { InputType, Field, OmitType } from '@nestjs/graphql';
+import { FileUpload, GraphQLUpload } from 'graphql-upload';
 
 @InputType()
-export class UpdateUserInput {}
+export class UpdateUserInput extends OmitType(User, ['id', 'chulaId', 'didSetup', 'isChulaStudent', 'profilePictureUrl']) {
+    @Field((_) => GraphQLUpload, { nullable: true })
+    profilePicture: Promise<FileUpload>;
+}
