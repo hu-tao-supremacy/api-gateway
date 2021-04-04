715742a54ac56a2b13da16b50b29e147ba0eb3dc
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-04 21:06:26 +0700

    feat(user): add updateUser mutation

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,9 +1,11 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { User } from '@entities/user.entity';
-import { NotImplementedException, UseGuards } from '@nestjs/common';
+import { ForbiddenException, UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { CurrentUser } from 'src/decorators/user.decorator';
 import { ProxyAccountService } from '@hu-tao-supremacy:account/proxy-account.service';
+import { UpdateUserInput } from '@inputs/user.input';
+import { merge } from 'lodash';
 
 @Resolver(() => User)
 export class UserResolver {
@@ -13,5 +15,18 @@ export class UserResolver {
   @Query(() => User)
   currentUser(@CurrentUser() user: User) {
     return this.proxyAccountService.getUserById(user.id)
+  }
+
+  @UseGuards(AuthGuard)
+  @Mutation(_ => User)
+  updateUser(@CurrentUser() currentUser: User, @Args('input') input: UpdateUserInput) {
+    const user = new User()
+    merge(user, input)
+
+    if (user.id !== currentUser.id) {
+      throw new ForbiddenException();
+    }
+
+    return this.proxyAccountService.updateAccountInfo(user)
   }
 }
