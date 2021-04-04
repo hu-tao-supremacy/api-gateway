1df6c30a372cbe470aee8c1057e6fa1be49bcb93
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-04 10:54:52 +0700

    feat(auth): add `firstname` and `lastname` to ChulaSSOSuccessResponse

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,48 +6,46 @@ import { AuthenticateOutput } from './auth.model';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    private readonly httpService: HttpService,
-    private readonly proxyAccountService: ProxyAccountService,
-  ) { }
+  constructor(private readonly httpService: HttpService, private readonly proxyAccountService: ProxyAccountService) {}
 
   serviceValidation(ticket: string): Observable<ChulaSSOSuccessResponse> {
     return this.httpService
-      .post<ChulaSSOSuccessResponse>(
-        'https://account.it.chula.ac.th/serviceValidation',
-        null,
-        {
-          headers: {
-            DeeAppId: process.env.CHULA_SSO_APP_ID,
-            DeeAppSecret: process.env.CHULA_SSO_APP_SECRET,
-            DeeTicket: ticket,
-          },
+      .post<ChulaSSOSuccessResponse>('https://account.it.chula.ac.th/serviceValidation', null, {
+        headers: {
+          DeeAppId: process.env.CHULA_SSO_APP_ID,
+          DeeAppSecret: process.env.CHULA_SSO_APP_SECRET,
+          DeeTicket: ticket,
         },
-      )
+      })
       .pipe(map((project) => project.data));
   }
 
   async authenticate(ticket: string): Promise<AuthenticateOutput> {
     const serviceAccount = await this.serviceValidation(ticket).toPromise();
-    console.log(serviceAccount)
+    console.log(serviceAccount);
 
-    if (serviceAccount) {
+    if (serviceAccount.uid !== '') {
       try {
-        const user = await this.proxyAccountService.getUserByChulaId(Number(serviceAccount.ouid)).pipe(catchError(error => {
-          console.log(error)
-          const firstName = serviceAccount.gecos.split(' ')[0]
-          const lastName = serviceAccount.gecos.split(' ')[1]
-          return this.proxyAccountService.createUser(firstName, lastName, serviceAccount.ouid)
-        })).toPromise()
+        const user = await this.proxyAccountService
+          .getUserByChulaId(Number(serviceAccount.ouid))
+          .pipe(
+            catchError((error) => {
+              console.log(error);
+              const firstName = serviceAccount.firstname;
+              const lastName = serviceAccount.lastname;
+              return this.proxyAccountService.createUser(firstName, lastName, serviceAccount.ouid);
+            }),
+          )
+          .toPromise();
         const accessToken = await this.proxyAccountService.generateAccessToken(user.id).toPromise();
-        return { accessToken }
+        return { accessToken };
       } catch (e) {
-        console.log(e)
+        console.log(e);
         throw new InternalServerErrorException();
       }
     }
 
-    throw new UnauthorizedException()
+    throw new UnauthorizedException();
   }
 
   isAuthenticated(accessToken: string): Observable<boolean> {
@@ -62,9 +60,6 @@ interface ChulaSSOSuccessResponse {
   email: string;
   roles: string[];
   ouid: string;
-}
-
-interface ChulaSSOFailureResponse {
-  type: string;
-  content: string;
+  firstname: string;
+  lastname: string;
 }
