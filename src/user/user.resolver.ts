3ecd4b94b1e15857940953bad3690c9ea676f777
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-04 09:51:53 +0700

    feat(user): add getUserById() to currentUser query

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -3,12 +3,15 @@ import { User } from '@entities/user.entity';
 import { NotImplementedException, UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { CurrentUser } from 'src/decorators/user.decorator';
+import { ProxyAccountService } from '@hu-tao-supremacy:account/proxy-account.service';
 
 @Resolver(() => User)
 export class UserResolver {
+  constructor(private readonly proxyAccountService: ProxyAccountService) { }
+
   @UseGuards(AuthGuard)
   @Query(() => User)
   currentUser(@CurrentUser() user: User) {
-    throw new NotImplementedException();
+    return this.proxyAccountService.getUserById(user.id)
   }
 }
