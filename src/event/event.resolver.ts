5b4ffbc5686effa2c3ba08b6db94d2ec4e721135
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-06 00:05:21 +0700

    fix(event): fix import statements

--- src/event/event.resolver.ts
+++ src/event/event.resolver.ts
@@ -1,22 +1,21 @@
 import { Args, Field, Int, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
 import { Event } from '@entities/event.entity';
 import { EventService } from './event.service';
-import { map } from 'rxjs/operators';
-import { ProxyOrganizerService } from '@onepass/organizer';
-import { ProxyParticipantService } from '@onepass/participant';
+import { OrganizerService } from '@onepass/organizer';
+import { ParticipantService } from '@onepass/participant';
 import { DateTime } from 'luxon';
 
 @Resolver((_) => Event)
 export class EventResolver {
   constructor(
-    private readonly proxyParticipantService: ProxyParticipantService,
-    private readonly proxyOrganizerService: ProxyOrganizerService,
+    private readonly participantService: ParticipantService,
+    private readonly organizerService: OrganizerService,
     private readonly eventService: EventService,
-  ) {}
+  ) { }
 
   @Query((_) => [Event])
   async upcomingEvents() {
-    return this.proxyParticipantService.getUpcomingEvents(
+    return this.participantService.getUpcomingEvents(
       DateTime.now().startOf('day').toISO(),
       DateTime.now().plus({ days: 14 }).endOf('day').toISO(),
     );
@@ -24,31 +23,31 @@ export class EventResolver {
 
   @Query((_) => Event)
   event(@Args('id', { type: () => Int }) id: number) {
-    return this.proxyParticipantService.getEventById(id);
+    return this.participantService.getEventById(id);
   }
 
   @ResolveField()
   organization(@Parent() event: Event) {
     const { organizationId } = event;
-    return this.proxyOrganizerService.getOrganizationById(organizationId);
+    return this.organizerService.getOrganizationById(organizationId);
   }
 
   @ResolveField()
   location(@Parent() event: Event) {
     const { locationId } = event;
     if (!locationId) return null;
-    return this.proxyParticipantService.getLocationById(locationId);
+    return this.participantService.getLocationById(locationId);
   }
 
   @ResolveField()
   tags(@Parent() event: Event) {
     const { id } = event;
-    return this.proxyParticipantService.getEventTags(id);
+    return this.participantService.getEventTags(id);
   }
 
   @ResolveField()
   durations(@Parent() event: Event) {
     const { id } = event;
-    return this.proxyParticipantService.getEventDurationsByEventId(id);
+    return this.participantService.getEventDurationsByEventId(id);
   }
 }
