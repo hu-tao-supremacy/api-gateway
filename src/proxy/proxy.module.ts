2a129ca8d03bd85d7bb73664e0822ee7d8fced29
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-05 21:37:21 +0700

    feat(proxy): generate NestJS services

--- src/proxy/proxy.module.ts
+++ src/proxy/proxy.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AccountService } from './account/account.service';
+import { FacilityService } from './facility/facility.service';
+import { OrganizerService } from './organizer/organizer.service';
+import { ParticipantService } from './participant/participant.service';
 
-@Module({})
+@Module({
+  providers: [AccountService, FacilityService, OrganizerService, ParticipantService]
+})
 export class ProxyModule {}
