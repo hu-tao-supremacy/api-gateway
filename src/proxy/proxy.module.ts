6e855b62cd262f94bd168caa4ab633b4e5d3923a
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-05 21:38:02 +0700

    feat(proxy): decorate ProxyModule with Global()

--- src/proxy/proxy.module.ts
+++ src/proxy/proxy.module.ts
@@ -4,7 +4,9 @@ import { FacilityService } from './facility/facility.service';
 import { OrganizerService } from './organizer/organizer.service';
 import { ParticipantService } from './participant/participant.service';
 
+@Global()
 @Module({
-  providers: [AccountService, FacilityService, OrganizerService, ParticipantService]
+  providers: [AccountService, FacilityService, OrganizerService, ParticipantService],
+  exports: [AccountService, FacilityService, OrganizerService, ParticipantService],
 })
 export class ProxyModule {}
