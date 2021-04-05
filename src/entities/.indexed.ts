05942d22bb162a8e1887da69a78dc26e1575cbe6
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-05 21:30:12 +0700

    feat(entities): add index.ts

---
+++ src/entities/.indexed.ts
@@ -0,0 +1,7 @@
+import { readdirSync, writeFileSync } from "fs";
+import { join } from "path";
+const data = readdirSync(join(__dirname, "."))
+  .filter((f) => f.search(".entity.ts") > 0)
+  .map((e) => `export * from './${e.split(".ts")[0]}'`)
+  .join("\n");
+writeFileSync("index.ts", data, { encoding: "utf8" });
