f3f249ebbdabbcc2bd6c885649c4407c6522f50d
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-05 21:43:34 +0700

    feat(adapters): add index.ts

---
+++ src/adapters/index.ts
@@ -0,0 +1,7 @@
+export * from './base.adapter'
+export * from './event-duration.adapter'
+export * from './event.adapter'
+export * from './location.adapter'
+export * from './organization.adapter'
+export * from './tag.adapter'
+export * from './user.adapter'
