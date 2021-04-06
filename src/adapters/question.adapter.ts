d390f1c56e721064952e6f651257a241513e33c9
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-06 17:10:21 +0700

    feat(adapters): add QuestionGroup + Question adapter

---
+++ src/adapters/question.adapter.ts
@@ -0,0 +1,7 @@
+import { BaseAdapter } from '@onepass/adapters';
+import { Question as QuestionIF } from '@onepass/api/common/common';
+import { Question } from '@onepass/entities';
+
+export class QuestionAdapter extends BaseAdapter<QuestionIF, Question> {
+  optionalFields = [];
+}
