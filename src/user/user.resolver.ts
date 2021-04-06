db3ddd388121aaeece7349a09b84b31e19827e24
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-06 16:34:28 +0700

    chore(user): update import statements

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,20 +1,20 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
-import { User } from '@entities/user.entity';
-import { ForbiddenException, UseGuards } from '@nestjs/common';
+import { User } from '@onepass/entities';
+import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { CurrentUser } from 'src/decorators/user.decorator';
-import { ProxyAccountService } from '@hu-tao-supremacy:account/proxy-account.service';
-import { UpdateUserInput } from '@inputs/user.input';
+import { AccountService } from '@onepass/account';
+import { UpdateUserInput } from '@onepass/inputs/user.input';
 import { merge } from 'lodash';
 
 @Resolver(() => User)
 export class UserResolver {
-  constructor(private readonly proxyAccountService: ProxyAccountService) { }
+  constructor(private readonly accountService: AccountService) { }
 
   @UseGuards(AuthGuard)
   @Query(() => User)
   currentUser(@CurrentUser() user: User) {
-    return this.proxyAccountService.getUserById(user.id)
+    return this.accountService.getUserById(user.id)
   }
 
   @UseGuards(AuthGuard)
@@ -23,6 +23,6 @@ export class UserResolver {
     const user = new User()
     merge(user, input)
     user.id = currentUser.id
-    return this.proxyAccountService.updateAccountInfo(user)
+    return this.accountService.updateAccountInfo(user)
   }
 }
