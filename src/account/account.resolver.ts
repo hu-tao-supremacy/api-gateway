96b9b37f4ba56177e319e63e3a5c890248d01cee
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-06 13:18:04 +0700

    chore(app): remove Account module

--- src/account/account.resolver.ts
+++
@@ -1,8 +0,0 @@
-import { User } from '@entities/user.entity';
-import { Resolver } from '@nestjs/graphql';
-import { ProxyAccountService } from 'src/proxy-account/proxy-account.service';
-
-@Resolver((_) => User)
-export class AccountResolver {
-  constructor(private readonly proxyAccountService: ProxyAccountService) { }
-}
