d66cbbb351b8b008be4c6e23928438f025587b97
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-07 11:19:01 +0700

    feat(user): add `events` field resolver

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,15 +1,16 @@
 import { Args, Mutation, Query, Resolver, ResolveField, Parent } from '@nestjs/graphql';
 import { User } from '@onepass/entities';
-import { UseGuards } from '@nestjs/common';
+import { UnauthorizedException, UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { CurrentUser } from 'src/decorators/user.decorator';
 import { AccountService } from '@onepass/account/account.service';
 import { UpdateUserInput } from '@onepass/inputs/user.input';
 import { merge } from 'lodash';
+import { ParticipantService } from '@onepass/participant/participant.service';
 
 @Resolver(() => User)
 export class UserResolver {
-  constructor(private readonly accountService: AccountService) { }
+  constructor(private readonly accountService: AccountService, private readonly participantService: ParticipantService) { }
 
   @UseGuards(AuthGuard)
   @Query(() => User)
@@ -18,15 +19,21 @@ export class UserResolver {
   }
 
   @UseGuards(AuthGuard)
-  @ResolveField()
-  events(@Parent() user: User) { }
-
-  @UseGuards(AuthGuard)
   @Mutation(_ => User)
   updateUser(@CurrentUser() currentUser: User, @Args('input') input: UpdateUserInput) {
     const user = new User()
     merge(user, input)
     user.id = currentUser.id
     return this.accountService.updateAccountInfo(user)
+  }
+
+  @UseGuards(AuthGuard)
+  @ResolveField()
+  events(@CurrentUser() currentUser: User, @Parent() user: User) {
+    if (currentUser.id !== user.id) {
+      throw new UnauthorizedException()
+    }
+
+    return this.participantService.getEventsByUserId(currentUser.id)
   }
 }
