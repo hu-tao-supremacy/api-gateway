9867a2e9e41c2598f47783e9a6a0d3b821dc6cb0
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-07 14:09:37 +0700

    feat(question-group): sort `questions` by `seq`

--- src/question-group/question-group.resolver.ts
+++ src/question-group/question-group.resolver.ts
@@ -1,13 +1,16 @@
 import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
 import { QuestionGroup } from '@onepass/entities';
 import { ParticipantService } from '@onepass/participant/participant.service';
+import { map } from 'rxjs/operators';
 
 @Resolver(() => QuestionGroup)
 export class QuestionGroupResolver {
-  constructor(private readonly participantService: ParticipantService) {}
+  constructor(private readonly participantService: ParticipantService) { }
 
   @ResolveField()
   questions(@Parent() questionGroup: QuestionGroup) {
-    return this.participantService.getQuestionsByQuestionGroupId(questionGroup.id);
+    return this.participantService.getQuestionsByQuestionGroupId(questionGroup.id).pipe(map(questions => {
+      return questions.sort((a, b) => a.seq - b.seq)
+    }));
   }
 }
