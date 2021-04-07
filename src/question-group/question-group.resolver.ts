cc305bfea9fec5a5b5232115357d63e54e766837
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-07 11:11:15 +0700

    feat(question-group): add `questions` field resolver

--- src/question-group/question-group.resolver.ts
+++ src/question-group/question-group.resolver.ts
@@ -1,4 +1,13 @@
-import { Resolver } from '@nestjs/graphql';
+import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
+import { QuestionGroup } from '@onepass/entities';
+import { ParticipantService } from '@onepass/participant/participant.service';
 
-@Resolver()
-export class QuestionGroupResolver {}
+@Resolver(() => QuestionGroup)
+export class QuestionGroupResolver {
+    constructor(private readonly participantService: ParticipantService) { }
+
+    @ResolveField()
+    questions(@Parent() questionGroup: QuestionGroup) {
+        return this.participantService.getQuestionsByQuestionGroupId(questionGroup.id)
+    }
+}
