2a65f35641c40bf9f0d7af5e9e41e2e0ebaadbab
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-07 15:19:17 +0700

    feat(user): check `previousProfilePictureUrl` before calling fileService.delete()

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -40,7 +40,7 @@ export class UserResolver {
       user.id = currentUser.id;
       user.profilePictureUrl = uri;
       return this.accountService.updateAccountInfo(user)
-    })).pipe(tap(_ => this.fileService.delete(previousProfilePictureUrl)))
+    })).pipe(tap(_ => previousProfilePictureUrl ?? this.fileService.delete(previousProfilePictureUrl)))
   }
 
   @UseGuards(AuthGuard)
