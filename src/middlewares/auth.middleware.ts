59a14ab8b4830d86d2dbc674b05f873f8e1f03fd
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-07 11:37:32 +0700

    chore(middlewares): run formatter

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,10 +1,10 @@
 import { User } from '@onepass/entities';
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { AuthService } from 'src/auth/auth.service';
-import { Request } from 'express'
-import { decode } from 'js-base64'
+import { Request } from 'express';
+import { decode } from 'js-base64';
 import { AccessTokenPayload } from '@onepass/graphql/account/service';
-import { AccountService } from '@onepass/account/account.service'
+import { AccountService } from '@onepass/account/account.service';
 
 declare global {
   namespace Express {
@@ -16,7 +16,7 @@ declare global {
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
-  constructor(private readonly authService: AuthService, private readonly accountService: AccountService) { }
+  constructor(private readonly authService: AuthService, private readonly accountService: AccountService) {}
 
   async use(req: Request, res: any, next: () => void) {
     try {
@@ -24,14 +24,14 @@ export class AuthMiddleware implements NestMiddleware {
       if (accessToken) {
         const isAuthenticated = await this.authService.isAuthenticated(accessToken).toPromise();
         if (isAuthenticated) {
-          const encodedPayload = accessToken.split('.')[1]
-          const decodedPayload = decode(encodedPayload)
-          const payload = JSON.parse(decodedPayload) as AccessTokenPayload
+          const encodedPayload = accessToken.split('.')[1];
+          const decodedPayload = decode(encodedPayload);
+          const payload = JSON.parse(decodedPayload) as AccessTokenPayload;
           const user = await this.accountService.getUserById(payload.userId).toPromise();
-          req.user = user
+          req.user = user;
         }
       }
-    } catch (_) { }
+    } catch (_) {}
     next();
   }
 }
