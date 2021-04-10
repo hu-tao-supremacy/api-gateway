f0ca43efba401441620039119344679523f9f649
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-10 18:12:25 +0700

    feat(inputs/user): add SetUserInterestsInput

--- src/inputs/user.input.ts
+++ src/inputs/user.input.ts
@@ -1,10 +1,10 @@
-import { Answer, User } from '@onepass/entities';
+import { Answer, Tag, User } from '@onepass/entities';
 import { InputType, Field, OmitType, PartialType, Int } from '@nestjs/graphql';
 import { FileUpload, GraphQLUpload } from 'graphql-upload';
 
 @InputType()
 export class UpdateUserInput extends PartialType(
-  OmitType(User, ['id', 'chulaId', 'didSetup', 'isChulaStudent', 'profilePictureUrl']),
+  OmitType(User, ['id', 'chulaId', 'didSetup', 'isChulaStudent', 'profilePictureUrl', 'interests']),
 ) {
   @Field((_) => GraphQLUpload, { nullable: true })
   profilePicture: Promise<FileUpload>;
@@ -26,4 +26,10 @@ export class CreateJoinRequestInput {
 export class DeleteJoinRequestInput {
   @Field(_ => Int)
   eventId: number;
+}
+
+@InputType()
+export class SetUserInterestsInput {
+  @Field(() => [Tag])
+  tags: Tag[]
 }
