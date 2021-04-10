b669ae16f59e6def4ec5a9094bd2684d58262c91
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-10 14:17:35 +0700

    feat(tag): add tags() query

--- src/tag/tag.resolver.ts
+++ src/tag/tag.resolver.ts
@@ -1,4 +1,21 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Int, Query, Resolver } from '@nestjs/graphql';
+import { Tag } from '@onepass/entities';
+import { ParticipantService } from '@onepass/participant/participant.service';
+import { Observable } from 'rxjs';
 
-@Resolver()
-export class TagResolver {}
+@Resolver(() => Tag)
+export class TagResolver {
+    constructor(private readonly participantService: ParticipantService) {}
+
+    @Query(() => [Tag])
+    tags(): Observable<Tag[]> {
+        return this.participantService.getTags()
+    }
+   
+    @Query(() => Tag)
+    tag(
+        @Args('id', { type: () => Int }) id: number
+    ): Observable<Tag> {
+        return this.participantService.getTagById(id)
+    }
+}
