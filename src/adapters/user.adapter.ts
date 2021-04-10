6288d31fc6e0bcc75401ef28855e95011e105cfd
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-10 22:24:29 +0700

    feat(adapters/user): add more values to optionalFields

--- src/adapters/user.adapter.ts
+++ src/adapters/user.adapter.ts
@@ -3,5 +3,5 @@ import { User as UserIF } from '@onepass/api/common/common';
 import { User as User } from '@onepass/entities';
 
 export class UserAdapter extends BaseAdapter<UserIF, User> {
-  optionalFields = ['chulaId', 'address', 'profilePictureUrl', 'nickname'];
+  optionalFields = ['phoneNumber', 'district', 'province', 'zipCode', 'chulaId', 'address', 'profilePictureUrl', 'nickname'];
 }
