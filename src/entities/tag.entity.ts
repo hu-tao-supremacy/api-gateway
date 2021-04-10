ed8f3dd683b9946f42fc1a79b0b49f9fe020c497
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-10 15:11:27 +0700

    fix(entities/tag): Event entity is not defined

--- src/entities/tag.entity.ts
+++ src/entities/tag.entity.ts
@@ -1,5 +1,6 @@
 import { Field, InputType, Int, ObjectType } from '@nestjs/graphql';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Event } from './event.entity';
 
 @InputType('TagInput')
 @ObjectType()
