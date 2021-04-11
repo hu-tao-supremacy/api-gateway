4b2e831ce65df9cfe056900995f4d581e73951a6
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-12 00:49:15 +0700

    fix(decorators): fix syntax error in if.decorator.ts

--- src/decorators/if.decorator.ts
+++ src/decorators/if.decorator.ts
@@ -1,6 +1,6 @@
 export function If(condition: boolean, decorator: MethodDecorator): MethodDecorator {
-  return (...args) {
-	if (condition) {
+  return (...args) => {
+	  if (condition) {
       return decorator(...args);
     }
   }
