c919e59ff470e15660c56d9fd4ffe88e3854a65b
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-12 20:29:04 +0700

    chore(attendance-context): update

--- src/attendance-context/attendance-context.resolver.ts
+++ src/attendance-context/attendance-context.resolver.ts
@@ -2,23 +2,7 @@ import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
 import { ParticipantService } from '@onepass/participant/participant.service';
 import { AttendanceContext } from 'src/entities/attendance-context.entity';
 
-@Resolver(() => AttendanceContext)
+@Resolver()
 export class AttendanceContextResolver {
     constructor(private readonly participantService: ParticipantService) {}
-
-    @ResolveField()
-    user(@Parent() context: AttendanceContext) {
-        const userId = context.userId;
-    }
-
-    @ResolveField()
-    attendance(@Parent() context: AttendanceContext) {
-        return this.participantService.getUserEvent(context.userId, context.eventId);
-    }
-
-    @ResolveField()
-    answers(@Parent() context: AttendanceContext) {
-        const userId = context.userId;
-        const eventId = context.eventId;
-    }
 }
