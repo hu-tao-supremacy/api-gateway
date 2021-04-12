498888e26124a96f1a026728049ca1a72a362494
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-12 17:02:44 +0700

    feat(attendance-context): add field resolvers

--- src/attendance-context/attendance-context.resolver.ts
+++ src/attendance-context/attendance-context.resolver.ts
@@ -1,4 +1,21 @@
-import { Resolver } from '@nestjs/graphql';
+import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
+import { AttendanceContext } from 'src/entities/attendance-context.entity';
 
-@Resolver()
-export class AttendanceContextResolver {}
+@Resolver(() => AttendanceContext)
+export class AttendanceContextResolver {
+    @ResolveField()
+    user(@Parent() context: AttendanceContext) {
+        const userId = context.userId;
+    }
+
+    @ResolveField()
+    attendance(@Parent() context: AttendanceContext) {
+        const attendanceId = context.attendanceId;
+    }
+
+    @ResolveField()
+    answers(@Parent() context: AttendanceContext) {
+        const userId = context.userId;
+        const eventId = context.eventId;
+    }
+}
