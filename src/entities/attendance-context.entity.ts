8f481eaa74e222a90be43adc6f54063efe39d43e
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-12 17:02:03 +0700

    feat(entities/attendance-context): add more fields

--- src/entities/attendance-context.entity.ts
+++ src/entities/attendance-context.entity.ts
@@ -1,10 +1,20 @@
-import { Field, ObjectType } from '@nestjs/graphql'
+import { Field, InputType, Int, ObjectType } from '@nestjs/graphql'
 import { Answer } from './answer.entity';
 import { Attendance } from './user-event.entity';
 import { User } from './user.entity'
 
+@InputType('AttendanceContextInput')
 @ObjectType()
 export class AttendanceContext {
+    @Field(() => Int)
+    userId: number;
+
+    @Field(() => Int)
+    attendanceId: number;
+
+    @Field(() => Int)
+    eventId: number;
+
     @Field(() => User)
     user: User;
 
