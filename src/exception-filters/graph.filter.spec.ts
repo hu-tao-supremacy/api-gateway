da5434d6ad1f1e2f8ec78d9d446a80f1cac75e62
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-13 12:57:16 +0700

    feat(main.ts): use GraphQLBaseExceptionFilter()

---
+++ src/exception-filters/graph.filter.spec.ts
@@ -0,0 +1,7 @@
+import { GraphFilter } from './graph.filter';
+
+describe('GraphFilter', () => {
+  it('should be defined', () => {
+    expect(new GraphFilter()).toBeDefined();
+  });
+});
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { GraphQLBaseExceptionFilter } from './exception-filters/graphql.exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new GraphQLBaseExceptionFilter())
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
