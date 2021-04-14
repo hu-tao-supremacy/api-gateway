75df1dc3bc830273169b175ff50feb06cb8beb36
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-14 14:44:18 +0700

    feat(user-org): add field resolvers for `organization` and `user`

--- src/user-organization/user-organization.resolver.ts
+++ src/user-organization/user-organization.resolver.ts
@@ -1,4 +1,24 @@
-import { Resolver } from '@nestjs/graphql';
+import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
+import { AccountService } from '@onepass/account/account.service';
+import { Organization, User, UserOrganization } from '@onepass/entities';
+import { OrganizerService } from '@onepass/organizer/organizer.service';
+import { ParticipantService } from '@onepass/participant/participant.service';
 
-@Resolver()
-export class UserOrganizationResolver {}
+@Resolver((of) => UserOrganization)
+export class UserOrganizationResolver {
+    constructor(
+        private readonly accountService: AccountService,
+        private readonly participantService: ParticipantService,
+        private readonly organizerService: OrganizerService
+    ) {}
+
+    @ResolveField(() => Organization)
+    organization(@Parent() userOrg: UserOrganization) {
+        return this.organizerService.getOrganizationById(userOrg.organizationId);
+    }
+
+    @ResolveField(() => User)
+    user(@Parent() userOrg: UserOrganization) {
+        return this.accountService.getUserById(userOrg.id)
+    }
+}
