f1526c72f364c0f89c9f4c41b5348ad531acd228
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-26 22:02:18 +0700

    feat(proxy/personalization): add initial implementation to PersonalizationService

--- src/proxy/personalization/personalization.service.ts
+++ src/proxy/personalization/personalization.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import {
+  HTS_PERSONALIZATION_PACKAGE_NAME,
+  PersonalizationServiceClient,
+  PERSONALIZATION_SERVICE_NAME,
+} from '@onepass/api/personalization/service';
 
 @Injectable()
-export class PersonalizationService {}
+export class PersonalizationService implements OnModuleInit {
+  private service: PersonalizationServiceClient;
+
+  constructor(@Inject(HTS_PERSONALIZATION_PACKAGE_NAME) private client: ClientGrpc) {}
+
+  onModuleInit() {
+    this.service = this.client.getService<PersonalizationServiceClient>(PERSONALIZATION_SERVICE_NAME);
+  }
+}
