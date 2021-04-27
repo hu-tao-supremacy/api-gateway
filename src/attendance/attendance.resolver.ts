0b8e1821495628e1aceb06362eba40ae20ae60c7
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-27 12:32:46 +0700

    feat(attendance): add field resolver for `user` and `event` fields

--- src/attendance/attendance.resolver.ts
+++ src/attendance/attendance.resolver.ts
@@ -1,13 +1,27 @@
 import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
-import { Answer, UserEvent } from '@onepass/entities';
+import { AccountService } from '@onepass/account/account.service';
+import { Answer, Event, User, UserEvent } from '@onepass/entities';
 import { ParticipantService } from '@onepass/participant/participant.service';
 
 @Resolver(() => UserEvent)
 export class AttendanceResolver {
-  constructor(private readonly participantService: ParticipantService) {}
+  constructor(
+    private readonly participantService: ParticipantService,
+    private readonly accountService: AccountService,
+  ) {}
 
   @ResolveField(() => [Answer])
   answers(@Parent() attendance: UserEvent) {
     return this.participantService.getAnswers(attendance.id);
+  }
+
+  @ResolveField(() => User)
+  user(@Parent() attendance: UserEvent) {
+    return this.accountService.getUserById(attendance.userId);
+  }
+
+  @ResolveField(() => Event)
+  event(@Parent() attendance: UserEvent) {
+    return this.participantService.getEventById(attendance.eventId);
   }
 }
