5c4055bcec6e4a8fa7d222e103fcab5c2fee74bb
FleeingSunlight <gh@fleeingsunlight.dev>
2021-05-01 20:48:12 +0700

    feat(tag): add `createTag` mutation

--- src/tag/tag.resolver.ts
+++ src/tag/tag.resolver.ts
@@ -1,11 +1,18 @@
-import { Args, Int, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
-import { Tag } from '@onepass/entities';
+import { UseGuards } from '@nestjs/common';
+import { Args, Int, Mutation, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import { Tag, User } from '@onepass/entities';
+import { OrganizerService } from '@onepass/organizer/organizer.service';
 import { ParticipantService } from '@onepass/participant/participant.service';
 import { Observable } from 'rxjs';
+import { CurrentUser } from 'src/decorators/user.decorator';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Resolver(() => Tag)
 export class TagResolver {
-  constructor(private readonly participantService: ParticipantService) {}
+  constructor(
+    private readonly participantService: ParticipantService,
+    private readonly organizerService: OrganizerService,
+  ) {}
 
   @Query(() => [Tag])
   tags(): Observable<Tag[]> {
@@ -20,5 +27,15 @@ export class TagResolver {
   @ResolveField(() => [Event])
   events(@Parent() tag: Tag) {
     return this.participantService.getEventsByTagId(tag.id);
+  }
+
+  @UseGuards(AuthGuard)
+  @Mutation(() => Tag)
+  createTag(
+    @CurrentUser() currentUser: User,
+    @Args('organizationId', { type: () => Int }) organizationId: number,
+    @Args('name') name: string,
+  ) {
+    return this.organizerService.createTag(currentUser.id, organizationId, name);
   }
 }
