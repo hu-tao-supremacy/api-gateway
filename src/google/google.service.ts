eab8dc226ac6b63f14f5ba051e402a2f5836afdc
FleeingSunlight <gh@fleeingsunlight.dev>
2021-05-02 21:53:24 +0700

    feat(google): add onModuleInit()

--- src/google/google.service.ts
+++ src/google/google.service.ts
@@ -1,20 +1,36 @@
-import { PubSub } from '@google-cloud/pubsub';
-import { Injectable } from '@nestjs/common';
+import { CreateSubscriptionOptions, PubSub } from '@google-cloud/pubsub';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 
 @Injectable()
-export class GoogleService {
+export class GoogleService implements OnModuleInit {
   private credentials = JSON.parse(process.env.GCP_CREDENTIALS);
   private pubSubClient = new PubSub({ credentials: this.credentials });
 
+  async onModuleInit() {
+    const TOPIC_NAME = 'personalization';
+    const SUBSCRIPTION_NAME = 'cloudrun';
+    const SUBSCRIPTION_PUSH_ENDPOINT = null;
+
+    // Create Pub/Sub topic if not exists.
+    await this.createPubSubTopic(TOPIC_NAME);
+
+    // Create Pub/Sub subscription if not exists.
+    await this.createPubSubSubscription(TOPIC_NAME, SUBSCRIPTION_NAME, {
+      pushEndpoint: SUBSCRIPTION_PUSH_ENDPOINT,
+    });
+  }
+
   async createPubSubTopic(name: string) {
     const exists = (await this.pubSubClient.topic(name).exists())[0];
-    if (!exists) {
-      this.pubSubClient.createTopic(name);
-    }
+    if (!exists) await this.pubSubClient.createTopic(name);
+  }
+
+  async createPubSubSubscription(topicName: string, subscriptionName: string, options: CreateSubscriptionOptions) {
+    const exists = (await this.pubSubClient.topic(topicName).subscription(subscriptionName).exists())[0];
+    if (!exists) await this.pubSubClient.topic(topicName).createSubscription(subscriptionName, options);
   }
 
   async generateVectorRepresentation(eventId: number): Promise<void> {
-    await this.createPubSubTopic('personalization');
     this.pubSubClient.topic('personalization').publishJSON({ eventId });
   }
 }
