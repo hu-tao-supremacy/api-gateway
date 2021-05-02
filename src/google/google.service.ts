19ab9fa7165e387b121d95faebf9a25268f6b7d7
FleeingSunlight <gh@fleeingsunlight.dev>
2021-05-02 17:36:15 +0700

    feat(google): add `generateVectorRepresentation` to GoogleService

--- src/google/google.service.ts
+++ src/google/google.service.ts
@@ -1,4 +1,12 @@
+import { PubSub } from '@google-cloud/pubsub';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class GoogleService {}
+export class GoogleService {
+  private credentials = JSON.parse(process.env.GCP_CREDENTIALS);
+  private pubSubClient = new PubSub({ credentials: this.credentials });
+
+  generateVectorRepresentation(eventId: number): void {
+    this.pubSubClient.topic('personalization').publishJSON({ eventId });
+  }
+}
