0f85a3e13a5169974e7c0d084ce7eed54d599e41
FleeingSunlight <gh@fleeingsunlight.dev>
2021-05-04 14:33:10 +0700

    chore(event): add console.log() statement

--- src/event/event.resolver.ts
+++ src/event/event.resolver.ts
@@ -240,13 +240,15 @@ export class EventResolver {
           this.fileService.upload(`events/${encode(`${createdEvent.id}`)}/covers/${nanoid()}`, input.coverImage),
           location
             ? of<number>(null)
-            : this.organizerService
-                .setEventLocation(currentUser.id, merge(new Location(), location))
-                .pipe(map((loc) => loc.id)),
+            : this.organizerService.setEventLocation(currentUser.id, merge(new Location(), location)).pipe(
+                tap((loc) => console.log(loc)),
+                map((loc) => loc.id),
+              ),
           durations ? this.setEventDurations(currentUser, { eventId: createdEvent.id, durations }) : of<number[]>([]),
         ]);
       }),
       switchMap(([updatedEvent, _, posterImage, coverImage, locationId]) => {
+        console.log(locationId);
         if (posterImage || coverImage || location) {
           updatedEvent.posterImageUrl = posterImage?.fileURI;
           updatedEvent.posterImageHash = posterImage?.hash;
